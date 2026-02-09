a948f7d3b4faa8bb312300409eb9590b48c2dd1b
Alvin9840 <alvin9840@gmail.com>
2026-02-08 20:03:00 -0500

    Week1 -just forget to stage the change on manager.py

--- src/database/manager.py
+++ src/database/manager.py
@@ -3,7 +3,7 @@ import psycopg2
 from psycopg2.extras import RealDictCursor
 
 DB_USER = os.getenv("DB_USER", "vtsaiber")
-DB_PASSWORD = os.getenv("DB_PASSWORD", "vtsaiber")
+DB_PASSWORD = os.getenv("DB_PASSWORD", "password")
 DB_HOST = os.getenv("DB_HOST", "localhost")
 DB_PORT = os.getenv("DB_PORT", "5432")
 DB_NAME = os.getenv("DB_NAME", "vtsaiber")
